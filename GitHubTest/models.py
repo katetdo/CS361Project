6f96398bdf84d1fa02dce66479baa78ddab7c9aa
alnoman612 <36910435+alnoman612@users.noreply.github.com>
2020-11-24 14:08:36 -0600

    Update models.py

--- GitHubTest/models.py
+++ GitHubTest/models.py
@@ -5,6 +5,11 @@ class MySyllabus(models.Model):
     assignmentWeight = models.CharField(max_length=20)
     gradeScale = models.CharField(max_length=20)
 
+class MyUser(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    type = models.CharField(max_length=1)
+    
 
 class PersonalInfo(models.Model):
     lastName = models.CharField(max_length=20)
@@ -13,14 +18,10 @@ class PersonalInfo(models.Model):
     officeNumber = models.CharField(max_length=20)
     email = models.CharField(max_length=20)
     phoneNumber = models.CharField(max_length=20)
+    syllabus = models.ForeignKey(MySyllabus, on_delete=models.CASCADE)
+    user = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 
-class MyUser(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    type = models.CharField(max_length=1)
-    syllabus = models.ForeignKey(MySyllabus, on_delete=models.CASCADE)
-    user = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
