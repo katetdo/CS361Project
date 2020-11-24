3a6c553f1346d19f51461979f0672bc7a5fec80d
alnoman612 <36910435+alnoman612@users.noreply.github.com>
2020-11-24 11:57:12 -0600

    Added the syllabus model.

--- GitHubTest/models.py
+++ GitHubTest/models.py
@@ -6,6 +6,13 @@ class MyUser(models.Model):
     password = models.CharField(max_length=20)
     type = models.CharField(max_length=1)
 
+
+class MySyllabus(models.Model):
+        assignmentWeight = models.CharField(max_length=20)
+        gradeScale = models.CharField(max_length=20)
+        user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+
+
 class PersonalInfo(models.Model):
     lastName = models.CharField(max_length=20)
     firstName = models.CharField(max_length=20)
