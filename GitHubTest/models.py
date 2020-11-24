75671e21f94d65d188f1be63f0b241cad304bb88
alnoman612 <36910435+alnoman612@users.noreply.github.com>
2020-11-24 10:27:25 -0600

    This is model branch stuff

--- GitHubTest/models.py
+++ GitHubTest/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
 # Create your models here.
+
+
+class MyUser(models.Model):
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+class PersonalInfo(models.Model):
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+
+
+class TA(models.Model):
+    personInfo = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+
+
+class Instructor(models.Model):
+    personInfo = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
