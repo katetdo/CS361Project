c0c885eeed4310d17a40fcabcf7d06cd55eedae6
alnoman612 <36910435+alnoman612@users.noreply.github.com>
2020-11-24 12:18:13 -0600

    Update models.py

--- GitHubTest/models.py
+++ GitHubTest/models.py
@@ -14,12 +14,6 @@ class MyUser(models.Model):
 
 
 
-class MySyllabus(models.Model):
-        assignmentWeight = models.CharField(max_length=20)
-        gradeScale = models.CharField(max_length=20)
-        user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-
-
 class PersonalInfo(models.Model):
     lastName = models.CharField(max_length=20)
     firstName = models.CharField(max_length=20)
