01910ba43e5ea3f5393296782cf3860cfacfaf96
alnoman612 <36910435+alnoman612@users.noreply.github.com>
2020-11-24 12:20:26 -0600

    I changes some stuff on the model based on how our database should be.

--- GitHubTest/models.py
+++ GitHubTest/models.py
@@ -2,15 +2,8 @@ from django.db import models
 
 
 class MySyllabus(models.Model):
-        assignmentWeight = models.CharField(max_length=20)
-        gradeScale = models.CharField(max_length=20)
-
-
-class MyUser(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    type = models.CharField(max_length=1)
-    syllabus = models.ForeignKey(MySyllabus, on_delete=models.CASCADE)
+    assignmentWeight = models.CharField(max_length=20)
+    gradeScale = models.CharField(max_length=20)
 
 
 class PersonalInfo(models.Model):
@@ -20,7 +13,14 @@ class PersonalInfo(models.Model):
     officeNumber = models.CharField(max_length=20)
     email = models.CharField(max_length=20)
     phoneNumber = models.CharField(max_length=20)
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+
+class MyUser(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    type = models.CharField(max_length=1)
+    syllabus = models.ForeignKey(MySyllabus, on_delete=models.CASCADE)
+    user = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
