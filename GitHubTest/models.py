7f4b80114b870b01ccb7088aa97f741aa9958d20
Connor Oaks <cjoaks@uwm.edu>
2020-11-24 10:45:27 -0600

    Added type field to MyUser, removed TA and instructor models.

--- GitHubTest/models.py
+++ GitHubTest/models.py
@@ -1,24 +1,15 @@
 from django.db import models
-# Create your models here.
 
 
 class MyUser(models.Model):
-    name = models.CharField(max_length=20)
+    username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
+    type = models.CharField(max_length=1)
+
 
 class PersonalInfo(models.Model):
     name = models.CharField(max_length=20)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-
-
-class TA(models.Model):
-    personInfo = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-
-
-
-class Instructor(models.Model):
-    personInfo = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
