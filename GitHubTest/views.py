d189e90253f6fdd03855ddc7cb6c46a2ffc39f51
Kate Do <do.kate12@gmail.com>
2020-12-08 13:53:17 -0600

    Changing TA and Instructor view to personal info view

--- GitHubTest/views.py
+++ GitHubTest/views.py
@@ -18,8 +18,8 @@ class Home(View):
             user_type = my_user.type
             if user_type == 'A':
                 return redirect("/administrator/")
-            elif user_type == 'I':
-                return redirect("/administrator/")
+            elif user_type == 'I' or user_type == 'T':
+                return redirect("/PersonalInfo/")
             else:
                 return redirect("/")
         except ObjectDoesNotExist:
@@ -51,53 +51,24 @@ class AdminView(View):
         return render(request, "admin.html", {"user_info": user_info, "name": name, "syllabi": syllabi})
 
 
-'''   def post(self,request):
-       try:
-           myAdmin = Admins.objects.get(name=request.POST['name'], password=request.POST['password'])
-          request.session["current"] = myAdmin.id
-          # to do...put where to redirect
-           return redirect("/Admin")
-       except Exception as e:
-
-
-           return render(request,"admin.html",{})
-    '''
-
-
-class TAView(View):
-    # Todo....
-    def get(self, request):
-        return render(request, "TA_UI_page1.html.html", {})
-
-        # Todo....
-        return render(request, "TA_UI_page2.html.html", {})
-
-    def post(self, request):
-        # try:
-        #     myuser = MyUser.objects.get(name=request.POST['name'], password=request.POST['password'], type=request.POST['type'])
-        #     request.session["current"] = myuser.id
-        #     # to do...put where to redirect
-        #     return redirect("/")
-        # except Exception as e:
-        #
-        # Todo....
-        return render(request, "TA_UI_page2.html.html", {})
-
-
-class InstructorView(View):
-    # Todo....
+class PersonalInfoView(View):
     def get(self, request):
         user = request.session.get("current", False)
-        userInfo = list(PersonalInfo.objects.filter(user=user))
-
-        return render(request, "admin.html", {"details": userInfo})
-
+        user_info = list(PersonalInfo.objects.filter(user=user))
+        return render(request, "TA_UI_page1.html", {"user_info": user_info})
 
     def post(self, request):
-        user = request.session.get("current", False)
-        first_name = request.POST.get('first_name', '')
+        user = PersonalInfo.objects.get(user=MyUser.objects.get(id=request.session["current"]))
+        name = user.firstName + " " + user.lastName
+        new_info = PersonalInfo(firstName=user.firstName,
+                                lastName=user.lastName,
+                                officeHours=request.POST["officeHours"],
+                                officeNumber=request.POST["officeNumber"],
+                                email=request.POST["email"],
+                                phoneNumber=request.POST["phoneNumber"],
+                                syllabus=user.syllabus,
+                                user=user)
 
+        new_info.save()
         return render(request, "TA_UI_page1.html.html", {})
-
-
 
