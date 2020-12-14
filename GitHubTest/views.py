f65b01781186f1f96690b7108b2b9ab0ec40b8c1
Connor Oaks <cjoaks@uwm.edu>
2020-12-13 18:43:17 -0600

    -Added the ability for instructors to delete syllabus components

--- GitHubTest/views.py
+++ GitHubTest/views.py
@@ -11,6 +11,7 @@ class Home(View):
         return render(request, "login.html", {"syllabi": syllabi, "error_msg": ""})
 
     def post(self, request):
+        msg = "Something went wrong. Please try again."
         if "login" in request.POST:
             try:
                 login = MyUserLogin.objects.get(username=request.POST['username'], password=request.POST['password'])
@@ -18,14 +19,12 @@ class Home(View):
                 request.session["current"] = my_user.id
                 return redirect("/Administrator/" if my_user.type == 'A' else "/PersonalInfo/")
             except ObjectDoesNotExist:
-                syllabi = list(MySyllabus.objects.all())
-                return render(request, "login.html", {"syllabi": syllabi,
-                                                      "error_msg": "Incorrect username or password."})
+                msg = "Incorrect username or password."
         if "guest" in request.POST:
             return redirect("/Syllabus/" + request.POST["course"])
         syllabi = list(MySyllabus.objects.all())
         return render(request, "login.html", {"syllabi": syllabi,
-                                              "error_msg": "Something went wrong. Please try again."})
+                                              "error_msg": msg})
 
 
 def build_course_info(course_objects):
@@ -48,7 +47,7 @@ def build_course_info(course_objects):
 class AdminView(View):
     def get(self, request):
         current_user = MyUser.objects.get(id=request.session["current"])
-        return render(request, "admin.html", self.get_template_data())
+        return render(request, "admin.html", get_admin_template_data())
 
     def post(self, request):
         if "create_user" in request.POST:
@@ -67,15 +66,16 @@ class AdminView(View):
                                     course=MyCourse.objects.get(id=request.POST["course"]),
                                     teachingAssistant=MyUser.objects.get(id=request.POST["teaching_assistant"]))
             new_section.save()
-        return render(request, "admin.html", self.get_template_data())
+        return render(request, "admin.html", get_admin_template_data())
 
-    def get_template_data(self):
-        return {
-            "tas": list(MyUser.objects.filter(type="T")),
-            "instructors": list(MyUser.objects.filter(type="I")),
-            "courses": list(MyCourse.objects.all().values()),
-            "course_info": build_course_info(MyCourse.objects.all())
-        }
+
+def get_admin_template_data():
+    return {
+        "tas": list(MyUser.objects.filter(type="T")),
+        "instructors": list(MyUser.objects.filter(type="I")),
+        "courses": list(MyCourse.objects.all().values()),
+        "course_info": build_course_info(MyCourse.objects.all())
+    }
 
 
 class PersonalInfoView(View):
@@ -95,8 +95,7 @@ class PersonalInfoView(View):
 
 class InstructorView(View):
     def get(self, request):
-        user = MyUser.objects.get(id=request.session["current"])
-        return render(request, "instructor.html", self.get_template_data(user))
+        return render(request, "instructor.html", get_instructor_template_data(request.session["current"]))
 
     def post(self, request):
         if "create_syllabus" in request.POST:
@@ -105,19 +104,44 @@ class InstructorView(View):
                                       courseName=request.POST["course"],
                                       courseDescription=request.POST["description"])
             new_syllabus.save()
+        elif "delete_syllabus" in request.POST:
+            syllabus = MySyllabus.objects.get(id=request.POST["syllabus"])
+            syllabus.delete()
         elif "add_component" in request.POST:
             new_component = MySyllabusComponent(syllabus=MySyllabus.objects.get(id=request.POST["syllabus"]),
                                                 name=request.POST["name"],
                                                 content=request.POST["contents"])
             new_component.save()
-        user = MyUser.objects.get(id=request.session["current"])
-        return render(request, "instructor.html", self.get_template_data(user))
+        elif "update_components" in request.POST:
+            return update_instructor_components(request)
+        elif "delete_component" in request.POST:
+            component = MySyllabusComponent.objects.get(id=request.POST["component"])
+            component.delete()
+        return render(request, "instructor.html", get_instructor_template_data(request.session["current"]))
 
-    def get_template_data(self, user):
-        return {
-            "syllabi": list(MySyllabus.objects.filter(instructor=user).values()),
-            "courses": list(MyCourse.objects.filter(instructor=user).values())
-        }
+
+def get_instructor_template_data(session_id):
+    user = MyUser.objects.get(id=session_id)
+    available_courses = []
+    for i in list(MyCourse.objects.filter(instructor=user)):
+        try:
+            MySyllabus.objects.get(course=i)
+        except ObjectDoesNotExist:
+            available_courses.append(i)
+    return {
+        "syllabi": list(MySyllabus.objects.filter(instructor=user).values()),
+        "available_courses": available_courses,
+        "selected_syllabus": "...",
+        "components": []
+    }
+
+
+def update_instructor_components(request):
+    data = get_instructor_template_data(request.session["current"])
+    selected_syllabus = MySyllabus.objects.get(id=request.POST["update_components"])
+    data["selected_syllabus"] = selected_syllabus.courseName
+    data["components"] = list(MySyllabusComponent.objects.filter(syllabus=selected_syllabus))
+    return render(request, "instructor.html", data)
 
 
 def render_syllabus(request, course):
@@ -128,5 +152,9 @@ def render_syllabus(request, course):
                                              "instructor": syllabus.instructor,
                                              "sections": sections,
                                              "components": components})
+
+
+
+
 
 
